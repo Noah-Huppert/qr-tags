d844d1e8223c4936b9545b24fe1044d2641ae064
Noah Huppert <noahhuppert@gmail.com>
2014-06-26 18:43:15 -0400

    Before paralax rewrite

--- public/js/qr-tag.js
+++ public/js/qr-tag.js
@@ -3,7 +3,66 @@ $(document).scroll(function(e){
 
   console.log(scroll);
 
-  if(scroll >= 60 && scroll <= 105){
+  var qrBack = $('#qrBack');
+  var qr = $('#qr img');
+  var smallHeader = $('#smallHeader');
+  var info = $('#info');
+
+  /*
+  Small Header Track
+    Start: 0
+      Do:
+        qrBack: None
+        qr: None
+        smallHeader: Map movement to just behind qr
+        info: None
+    End: 46
+      Do:
+        qrBack: None
+        qr: None
+        smallHeader: Stop map
+        info: None
+        
+  Small Header Show
+    Start: 46
+      Do:
+        qrBack: resize with scroll
+        qr: None
+        smallHeader: none
+        info: None
+    End: 107
+      Do:
+        qrBack: Stop resize
+        qr: none
+        smallHeader: Lock movement to qrBack
+        info: None
+
+  Top Bump
+    Start: 170
+      Do:
+        qrBack: move down @ 1/3 scroll
+        qr: move down @ 1/3 scroll
+        smallHeader: move down @ 1/3
+        info: None
+    End: 458
+      Do:
+        qrBack: Lock movement
+        qr: Lock movement
+        smallHeader: Lock movement
+        info: Lock movement
+
+  Info scroll
+    Start: 458
+      Do:
+        qrBack: None
+        qr: None
+        smallHeader: None
+        info: Map scroll to padding-bottom
+    End: Never
+      Do: N/A
+  */
+
+  /*if(scroll >= 60 && scroll <= 105){
     $('#qrBack').height(300 + (scroll - 60));
   } else if(scroll >= 105){
     $('#qrBack').height(345);
@@ -11,7 +70,7 @@ $(document).scroll(function(e){
     $('#qrBack').height(300);
   }
 
-  if(scroll >= 171 && scroll <= 495){/* FIX AT 390 scroll */
+  if(scroll >= 171 && scroll <= 495){
     $('#qrBack').css({
       'position': 'fixed',
       'margin-top': -456 + ((scroll - 172.5) / 3) + 436,
@@ -48,15 +107,20 @@ $(document).scroll(function(e){
     });
   }
 
-  if(scroll >= 105){
+  if(scroll >= 105 && scroll <= 171){
     $('#smallHeader').css({
       'position': 'absolute',
       'top': '105px'
+    });
+  } else if(scroll >= 171 && scroll <= 495){
+    $('#smallHeader').css({
+      'top': $('#qr img').offset().top,
+      'z-index': '5'
     });
   } else{
     $('#smallHeader').css({
       'position': 'fixed',
       'top': '0px'
     });
-  }
+  }*/
 });
