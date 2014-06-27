1328dcbc5cf1be802f5ab36d175ed405f5aaf4f9
Noah Huppert <noahhuppert@gmail.com>
2014-06-26 21:39:41 -0400

    After correct revert

--- public/js/qr-tag.js
+++ public/js/qr-tag.js
@@ -60,4 +60,60 @@ $(document).scroll(function(e){
     End: Never
       Do: N/A
   */
+  if(scroll >= 60 && scroll <= 105){
+    $('#qrBack').height(300 + (scroll - 60));
+  } else if(scroll >= 105){
+    $('#qrBack').height(345);
+  } else{
+    $('#qrBack').height(300);
+  }
+
+  /*if(scroll >= 171 && scroll <= 495){
+    $('#qrBack').css({
+      'position': 'fixed',
+      'margin-top': -456 + ((scroll - 172.5) / 3) + 436,
+      'z-index': '3'
+    });
+
+    $('#qr img').css({
+      'position': 'fixed',
+      'margin-top': (scroll - 172.5) / 3
+    });
+
+    $('#info').css({
+      'margin-top': '556px'
+    });
+  } else if(scroll >= 495){
+    $('#qrBack').css({
+      'position': 'fixed'
+    });
+  } else{
+    $('#qrBack').css({
+      'position': 'relative',
+      'margin-top': '-285px',
+      'z-index': '2'
+    });
+
+    $('#qr img').css({
+      'position': 'relative',
+      'margin-top': '170px'
+    });
+
+    $('#info').css({
+      'margin-top': '50px',
+      'position': 'relative'
+    });
+  }*/
+
+  if(scroll >= 105){
+    $('#smallHeader').css({
+      'position': 'absolute',
+      'top': '105px'
+    });
+  } else{
+    $('#smallHeader').css({
+      'position': 'fixed',
+      'top': '0px'
+    });
+  }
 });
